d3a5755be6eeb52cafb1417ba289f17202248690
RuslanKulik <40599004+ruslanvgu@users.noreply.github.com>
2021-09-03 09:45:11 +0300

    Create stack.c

---
+++ stack.c
@@ -0,0 +1,59 @@
+*
+data structure - stack
+language - c, c++
+
+
+*/
+
+
+
+
+#include <stack>
+
+
+
+
+int main()
+{
+    /* --------создание и заполнение stack-------- */
+    stack<int,list<int>> s;   /* Объявляем стек с указанием контейнера который лежит в основе*/
+    s.push(1);                 /* метод push создает  копию элемента и переносит  ее в стек */
+    s.emplace(4);              /* метод emplace создает элемент  неповсредственно в коллекции(в стеке), без копирования и  перемещения. т.о. пыволняется быстрее.
+    
+    /* --------печать -----------------------------*/
+    print_from_while(s);
+
+      
+      
+      
+      
+      
+      return 0;
+}
+
+/* обычная функция печати через цикл while */
+void print_from_while( std::stack<int,std::list<int>>  s)
+{
+    while(!s.empty())
+    {
+        std::cout << s.top()<<" ";
+        s.pop();
+    }
+    std::cout << std::endl;
+}
+
+/* функция печати через внутренний контейнет  */
+template < typename T >
+void print_from_inner_container( const std::stack<T>& stk )
+{
+    struct cheat : std::stack<T>                            /* определение нового класса стек через наследование   */
+    {
+        using std::stack<T>::c;                             /* метод доступа к внутреннему контейнеру */ 
+    } ;
+    const auto& seq = static_cast< const cheat& >(stk).c;  /* получение внутреннего контейнера */
+
+    for( const auto& v : seq )                              /* обычная печать через контейнера */
+        std::cout << v << ' ' ;
+        
+    std::cout << '\n' ;
+}

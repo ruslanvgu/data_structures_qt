8c67f72eef4ca3faf0634db0c8f20b6de4614852
RuslanKulik <40599004+ruslanvgu@users.noreply.github.com>
2021-09-10 14:49:39 +0300

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -5,7 +5,7 @@ typedef struct node
     struct node * l ;
 } Node;
 
-Node* add(Node** top, int value)
+void add(Node** top, int value)
 {
     if( (*top) == NULL )
     {
@@ -17,8 +17,10 @@ Node* add(Node** top, int value)
     {
         if((*top)->x > value)
             add(&(*top)->l,value);
-        else
+        else if((*top)->x < value)
             add(&(*top)->r,value);
+        else
+            return;
     }
 }
 
@@ -26,9 +28,90 @@ void print(Node *elem)
 {
     if(elem == NULL) return;
     else{
-            print(elem->l);
-            printf("%d ",elem->x);
-            print(elem->r);
+        print(elem->l);
+        printf("%d ",elem->x);
+        print(elem->r);
+    }
+}
+
+Node* find_node(Node* e, int v)
+{
+    if(e == NULL) return NULL;
+    else {
+        if(e->x == v) return e;
+        else if( e->x > v)
+            find_node(e->l,v);
+        else
+            find_node(e->r,v);
+    }
+}
+
+void del(Node *e, int v)
+{
+    if(e == NULL ) return;
+    else{
+        Node* prev = NULL;
+        while(e && e->x != v)
+        {
+            prev = e;
+            if(e->x > v)
+                e = e->l;
+            else
+                e = e->r;
+        }
+
+        if(!e) return;
+
+        if(e->l == NULL && e->r == NULL)//dont have children
+        {
+            if(prev->x > e->x)
+                prev->l = NULL;
+            else
+                prev->r = NULL;
+            free(e);
+            e = NULL;
+
+        }else if(e->l == NULL )//have 1 child right
+        {
+            Node *tmp = e->r;
+            free(e);
+            if(prev->x > tmp->x)
+                prev->l = tmp;
+            else
+                prev->r = tmp;
+
+        }else if( e->r == NULL)//have 1 child left
+        {
+            Node *tmp = e->l;
+            free(e);
+            if(prev->x > tmp->x)
+                prev->l = tmp;
+            else
+                prev->r = tmp;
+        }
+        else//have 2 child
+        {
+            //find min elem right branch
+            Node * min = e->r;
+            Node *prevMin = NULL;
+            while( min->l != NULL){
+                prevMin = min;
+                min = min->l;
+            }
+
+            prevMin->l = min->r;
+
+            min->l = e->l;
+            min->r = e->r;
+
+            free(e);
+            e = NULL;
+
+            if(prev->x > min->x)
+                prev->l = min;
+            else
+                prev->r = min;
+        }
     }
 }
 
@@ -36,15 +119,19 @@ int main(int, char**)
 {
     using namespace std;
 
-
     Node *head = NULL;
-    add(&head,4);
+    add(&head,7);
     add(&head,2);
-    add(&head,3);
-    add(&head,1);
+    add(&head,9);
+    add(&head,8);
+    add(&head,11);
+    add(&head,10);
+    add(&head,12);
     print(head);
-
-
+    printf("\n");
+    del(head,2);
+    print(head);
+    printf("\n");
     return 0;
 }
 
